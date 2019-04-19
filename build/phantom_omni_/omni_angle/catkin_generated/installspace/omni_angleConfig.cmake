e282213304f87f50a25c517a21797a49283bb1a3
KID0616 <kohei16yamamoto2@gmail.com>
2019-04-19 17:21:24 +0900

    a

--- build/phantom_omni_/omni_angle/catkin_generated/installspace/omni_angleConfig.cmake
+++ build/phantom_omni_/omni_angle/catkin_generated/installspace/omni_angleConfig.cmake
@@ -66,7 +66,7 @@ endif()
 set(omni_angle_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
-if("TRUE" STREQUAL "TRUE")
+if("FALSE" STREQUAL "TRUE")
   set(omni_angle_SOURCE_PREFIX /home/kid/catkin_ws/src/phantom_omni_/omni_angle)
   set(omni_angle_DEVEL_PREFIX /home/kid/catkin_ws/devel)
   set(omni_angle_INSTALL_PREFIX "")
@@ -110,7 +110,7 @@ if(NOT " " STREQUAL " ")
         message(FATAL_ERROR "Project 'omni_angle' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'omni_angle' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/kid/catkin_ws/src/phantom_omni_/omni_angle/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'omni_angle' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/kid/catkin_ws/install/${idir}'.  ${_report}")
     endif()
     _list_append_unique(omni_angle_INCLUDE_DIRS ${include})
   endforeach()
@@ -129,7 +129,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/kid/catkin_ws/devel/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/kid/catkin_ws/install/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
---
+++ build/phantom_omni_/phantom_omni_/cmake/phantom_omni-genmsg-context.py
@@ -0,0 +1,11 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = "/home/kid/catkin_ws/src/phantom_omni_/phantom_omni_/msg/PhantomButtonEvent.msg;/home/kid/catkin_ws/src/phantom_omni_/phantom_omni_/msg/OmniFeedback.msg"
+services_str = ""
+pkg_name = "phantom_omni"
+dependencies_str = "geometry_msgs"
+langs = "gencpp;geneus;genlisp;gennodejs;genpy"
+dep_include_paths_str = "phantom_omni;/home/kid/catkin_ws/src/phantom_omni_/phantom_omni_/msg;geometry_msgs;/opt/ros/kinetic/share/geometry_msgs/cmake/../msg;std_msgs;/opt/ros/kinetic/share/std_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "/usr/bin/python"
+package_has_static_sources = '' == 'TRUE'
+genmsg_check_deps_script = "/opt/ros/kinetic/share/genmsg/cmake/../../../lib/genmsg/genmsg_check_deps.py"
